eca48fc607175eeddf64fd329d79d1519a75c834
William John Atkin <WJAtkin@UCLac.uk>
2018-02-12 10:45:08 +0000

    Fully implemented CUISprite

--- CO2403-Professional-Skills/CO2403-Professional-Skills.cpp
+++ CO2403-Professional-Skills/CO2403-Professional-Skills.cpp
@@ -1,7 +1,18 @@
 // CO2403-Professional-Skills.cpp: A program using the TL-Engine
 
-#include <TL-Engine.h>	// TL-Engine include file and namespace
+#include "BUILD_ORDER.h"
 using namespace tle;
+
+// Tempory Keys
+EKeyCode G_PAN_FORWARDS = Key_W;
+EKeyCode G_PAN_BACKWARDS = Key_S;
+EKeyCode G_PAN_RIGHT = Key_D;
+EKeyCode G_PAN_LEFT = Key_A;
+EKeyCode G_EXIT = Key_Escape;
+
+// Tempory speed
+const float G_GAME_SPEED = 1.0f;
+const float G_UI_MOVE_SPEED = 250.0f;
 
 void main()
 {
@@ -11,18 +22,42 @@ void main()
 
 	// Add default folder for meshes and other media
 	myEngine->AddMediaFolder( "C:\\ProgramData\\TL-Engine\\Media" );
+	myEngine->AddMediaFolder( "TestMedia" );
 
 	/**** Set up your scene here ****/
+	CUISprite* artUI = new CUISprite(myEngine, "AnArt.png", { 20.0f, 50.0f } );
 
-
+	float deltaTime;
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
 	{
 		// Draw the scene
+		deltaTime = myEngine->Timer();
 		myEngine->DrawScene();
 
 		/**** Update your scene each frame here ****/
 
+		if (myEngine->KeyHeld(G_PAN_FORWARDS))
+		{
+			artUI->MoveY(G_UI_MOVE_SPEED * deltaTime * G_GAME_SPEED);
+		}
+		else if (myEngine->KeyHeld(G_PAN_BACKWARDS))
+		{
+			artUI->MoveY(-G_UI_MOVE_SPEED * deltaTime * G_GAME_SPEED);
+		}
+		if (myEngine->KeyHeld(G_PAN_RIGHT))
+		{
+			artUI->MoveX(G_UI_MOVE_SPEED * deltaTime * G_GAME_SPEED);
+		}
+		else if (myEngine->KeyHeld(G_PAN_LEFT))
+		{
+			artUI->MoveX(-G_UI_MOVE_SPEED * deltaTime * G_GAME_SPEED);
+		}
+
+		if (myEngine->KeyHit(G_EXIT))
+		{
+			myEngine->Stop();
+		}
 	}
 
 	// Delete the 3D engine now we are finished with it
--- CO2403-Professional-Skills/CUISprite.h
+++ CO2403-Professional-Skills/CUISprite.h
@@ -4,11 +4,41 @@
 class CUISprite : public CBaseSprite
 {
 public:
+	// Constructers
 	CUISprite(tle::I3DEngine* pEngine, const char* pSpriteName, SVector3D<float> position);
 	CUISprite(tle::I3DEngine* pEngine, const char* pSpriteName, SVector2D<float> position);
+	// Destructers
 	~CUISprite();
 
+	// For sprite movement
+	// By translation
+	void Move(SVector3D<float> moveAmount);
+	void Move(SVector2D<float> moveAmount);
+	void MoveX(float moveAmount);
+	void MoveY(float moveAmount);
+	void MoveZ(float moveAmount);
+	// By setting
+	void SetPosition(SVector3D<float> moveAmount);
+	void SetPosition(SVector2D<float> moveAmount);
+	void SetX(float newX);
+	void SetY(float newY);
+	void SetZ(float newZ);
+	// By resetting
+	void ResetPosition();
+
+	// Getters
+	SVector3D<float> GetPosition3D();
+	SVector2D<float> GetPosition2D();
+	float GetX();
+	float GetY();
+	float GetZ();
+
 private:
+
+	// This UIs sprite
 	tle::ISprite* mpSprite;
 
+	// Z position that the UI will default to if one
+	// is not specifiyed
+	const float DEFAULT_Z_POS = 0.5f;
 };
