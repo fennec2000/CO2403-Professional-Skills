52e83d24e430f63117ec6f84efd28fb4acd90fae
WagonDredge <anthonymarsh@fastmail.co.uk>
2018-02-28 12:14:10 +0000

    something

--- CO2403-Professional-Skills/CCore.h
+++ CO2403-Professional-Skills/CCore.h
@@ -6,11 +6,7 @@ using namespace tle;
 // This holds data core to the game that needs to be passed arround
 
 class CPlayer;
-<<<<<<< HEAD
 class CEProjectile;
-=======
-class CLevel;
->>>>>>> master
 
 enum EPlayers { Player1, Player2, NumOfEPlayers };
 
@@ -24,7 +20,6 @@ private:
 	I3DEngine* mTLEngine;	// pointer to the tl engine
 	float mFrameTime;		// time between each frame
 	CPlayer* mpPlayer[EPlayers::NumOfEPlayers];		// holds pointers to the players - possiable 2 player
-	CLevel* mpLevel; // A pointer to the games level
 	// vector<enemies>		// holds a list of enemies
 	
 
@@ -34,24 +29,17 @@ private:
 public:
 	//Static access method.
 	static CCore* GetInstance();
-<<<<<<< HEAD
 	vector<CEProjectile*> eBullets;
 	// Public functions
-=======
-
-	// Public function
->>>>>>> master
 	void UpdateCore();
 
 	// Getters
 	I3DEngine* GetTLEngine() { return mTLEngine; };
 	float* GetFrameTimer() { return &mFrameTime; };
 	CPlayer* GetPlayer(EPlayers player) { return mpPlayer[player]; };
-	CLevel* GetLevel() { return mpLevel; };
 
 	// Setters
 	void AddPlayer(EPlayers player, CPlayer &givenPlayer);
 	void AddBullet(float ex, float ey, float ez);
 	void updateBullets();
-	void removeBullets();
 };
