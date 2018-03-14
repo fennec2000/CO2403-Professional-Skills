2169fb57d402f5f384e0fb5d04e37bee434eb59b
fennec2000 <stuart.alh@gmail.com>
2018-03-14 11:06:09 +0000

    merge test fix

--- CO2403-Professional-Skills/CCore.cpp
+++ CO2403-Professional-Skills/CCore.cpp
@@ -43,7 +43,7 @@ void CCore::UpdateCore()
 	// Draw the scene
 	mFrameTime = pTLEngine->Timer();	// update the frame timer
 	pTLEngine->DrawScene();				// draw the frame
-	mpLevel->Update();
+	pLevel->Update();
 
 	for (std::vector<CEProjectile*>::iterator it = eBullets.begin(); it != eBullets.end(); it++)
 	{
